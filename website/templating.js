79bab163ca4471a2a6f14fb27e3c46f87cfac3a9
Pedro Binotto <pedrosantibinotto@gmail.com>
2024-10-10 20:23:45 -0300

    aksdlakd

--- website/templating.js
+++ website/templating.js
@@ -2,13 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
   fetch('/layout.html')
     .then(response => response.text())
     .then(layout => {
+      const body = document.getElementsByTagName("body")
+      const content = document.getElementById("content")
+
       console.log("body", body)
       console.log("content", content)
       console.log("document", document)
       console.log("layout", layout)
 
-      const body = document.getElementsByTagName("body")
-      const content = document.getElementById("content")
       layout.getElementById('main').appendChild(content)
       body.replaceChildren(layout)
     })
