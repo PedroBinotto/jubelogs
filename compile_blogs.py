695cc2036338fd06fc30a37f168b5db2f2eea6f6
Pedro Binotto <pedrosantibinotto@gmail.com>
2024-11-24 04:16:11 -0300

    contents box

--- compile_blogs.py
+++ compile_blogs.py
@@ -110,8 +110,16 @@ def convert_post(src_path: str, target_folder: str) -> Blog:
         transpiled_soup.insert(0, header)
 
     title_element = transpiled_soup.find("h1")
+    content_container = soup_from_path("./templates/blog_contents.html").find(
+        id="blog-contents-container"
+    )
     category = extract_category(src_path)
     synopsis = transpiled_soup.find("p").string
+
+    for element in title_element.find_all_next():
+        content_container.append(element.extract())
+
+    title_element.insert_after(content_container)
 
     if category:
         category_tag = transpiled_soup.new_tag("h3")
